079c9f1c0e4f2784b8abd348921ceeb05c02abcf
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-09-13 01:34:38 +0100

    Upload router.js

--- stage-two/routes/router.js
+++ stage-two/routes/router.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const dataRequest = require('../controller/controller');
 
 router.post('/api', dataRequest);
+router.get('/api/:user_id', dataRequest);
 router.put('/api/:user_id', dataRequest);
 router.delete('/api/:user_id', dataRequest);
-router.get('/api/:user_id', dataRequest);
 
 
 module.exports = router;
